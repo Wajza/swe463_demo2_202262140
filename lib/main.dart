0530d941266bbb452888e9c2b6f35cf5c9b1b398
Wajza <s202262140@kfupm.edu.sa>
2026-09-01 12:48:15 +0300

    Asynchronous Programming part 2

--- lib/main.dart
+++ lib/main.dart
@@ -28,25 +28,19 @@
 // [6] .then() result: Fetched Data
 
 //  8. Asynchronous Programming — 11 points
-Future<String> fetchData() async {
-  await Future.delayed(const Duration(seconds: 2));
-  return 'Fetched Data';
+Stream<int> countStream(int to) async* {
+  for (int i = 1; i <= to; i++) {
+    await Future.delayed(const Duration(seconds: 1));
+    yield i;
+  }
 }
 
 Future<void> main() async {
-  print('[1] Using async/await');
-  print('[2] Fetching data...');
+  print('Start counting...');
 
-  final data = await fetchData();
-  print('[3] Result: $data');
+  await for (final count in countStream(3)) {
+    print(count);
+  }
 
-  print('[4] Using .then()');
-
-  fetchData().then((value) {
-    print('[6] .then() result: $value');
-  }).catchError((error) {
-    print('[7] Error: $error');
-  });
-
-  print('[5] Program continues after .then() call');
+  print('Done');
 }
