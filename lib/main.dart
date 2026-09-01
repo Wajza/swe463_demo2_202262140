34b66a82a402ca2a2c9c01e4c0c53798f1bcf259
Wajza <s202262140@kfupm.edu.sa>
2026-09-01 11:40:58 +0300

    Variable Modifiers

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
-//  1. Variables and Data Types — 8 points
+// 1. What is the difference between `final` and `const`?
+// `final` is a variable that can only be set once and is initialized at runtime, while `const` is a compile time constant that must be known at compile time and cannot change.
+
+// 2. Why can `dynamic` change from `String` to `int`?
+// `dynamic` is a type that can hold any value and can change its type at runtime. It allows for flexibility in variable assignment, enabling it to hold different types of values during the execution of the program.
+
+//  2. Variable Modifiers — 7 points
 void main() {
-  int age = 23;
-  double height = 1.50;
-  bool isStudent = true;
-  String name = 'Wajd';
-  List<int> scores = [90, 80, 70];
+  final String courseName = 'Mobile App Development';
+  const double pi = 3.14159;
 
-  var city = 'Dhahran';
+  dynamic anything = 'I can be any type!';
+  courseName = 'Web Development';
+  pi = 3.14;
+  print(courseName);
+  print(pi);
+  print(anything);
 
-  print('$name is $age years old and $height meters tall.');
-  print('Student: $isStudent');
-  print('Scores: $scores');
-  print('City: $city');
+  anything = 42;
+  print(anything);
 }
