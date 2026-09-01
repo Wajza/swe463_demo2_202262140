e909fa2ecf5354ba5e6bbe1e18fad6591f6cbbf7
Wajza <s202262140@kfupm.edu.sa>
2026-09-01 12:56:26 +0300

    Final Exercise

--- lib/main.dart
+++ lib/main.dart
@@ -27,20 +27,77 @@
 // [5] Program continues after .then() call
 // [6] .then() result: Fetched Data
 
-//  8. Asynchronous Programming — 11 points
-Stream<int> countStream(int to) async* {
-  for (int i = 1; i <= to; i++) {
-    await Future.delayed(const Duration(seconds: 1));
-    yield i;
-  }
-}
+// Fill in this table:
 
-Future<void> main() async {
-  print('Start counting...');
+// | Dart feature | Line number(s) |
+// |---|---|
+// | Variables and types | 19, 26, 27 |
+// | Null safety | 27, 47 |
+// | Function definitions | 3, 9, 22, 29, 37 |
+// | Collections | 	45–48 |
+// | Class definitions | 5, 16, 25 |
+// | Generics | 22, 25 |
+// | Positional parameter definitions | 9, 37 |
+// | Named parameter definitions | 6, 17 |
+// | Private members | 26, 27, 25, 29 (the _MyHomePageState, _counter, _incrementCounter identifiers) |
+// | Importing packages | 1 |
+// | Inheritance | 5, 16, 25 |
 
-  await for (final count in countStream(3)) {
-    print(count);
-  }
+//  Final Exercise — Identify Dart Features in Flutter Code — 5 points
+import 'package:flutter/material.dart';                       // 1
 
-  print('Done');
-}
+void main() => runApp(const MyApp());                       // 3
+
+class MyApp extends StatelessWidget {                       // 5
+  const MyApp({super.key});                                 // 6
+
+  @override                                                  // 8
+  Widget build(BuildContext context) {                      // 9
+    return const MaterialApp(                               // 10
+      home: MyHomePage(title: 'Flutter Demo'),              // 11
+    );                                                       // 12
+  }                                                          // 13
+}                                                            // 14
+
+class MyHomePage extends StatefulWidget {                   // 16
+  const MyHomePage({super.key, required this.title});       // 17
+
+  final String title;                                       // 19
+
+  @override                                                  // 21
+  State<MyHomePage> createState() => _MyHomePageState();    // 22
+}                                                            // 23
+
+class _MyHomePageState extends State<MyHomePage> {          // 25
+  int _counter = 0;                                         // 26
+  String? message;                                          // 27
+
+  void _incrementCounter() {                                // 29
+    setState(() {                                           // 30
+      _counter++;                                           // 31
+      message = 'Button pressed';                           // 32
+    });                                                      // 33
+  }                                                          // 34
+
+  @override                                                  // 36
+  Widget build(BuildContext context) {                      // 37
+    return Scaffold(                                        // 38
+      appBar: AppBar(                                       // 39
+        title: Text(widget.title),                          // 40
+      ),                                                     // 41
+      body: Center(                                         // 42
+        child: Column(                                      // 43
+          mainAxisAlignment: MainAxisAlignment.center,      // 44
+          children: <Widget>[                               // 45
+            Text('Count: $_counter'),                       // 46
+            Text(message ?? 'Press the button'),            // 47
+          ],                                                 // 48
+        ),                                                   // 49
+      ),                                                     // 50
+      floatingActionButton: FloatingActionButton(           // 51
+        onPressed: _incrementCounter,                       // 52
+        child: const Icon(Icons.add),                       // 53
+      ),                                                     // 54
+    );                                                       // 55
+  }                                                          // 56
+}                                                            // 57
